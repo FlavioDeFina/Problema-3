773dd1582bd37aac77afea3209d789013231fa8d
José Manuel <Jos Manuel@DESKTOP-L67G7N4>
2023-08-28 21:34:07 -0500

    Agregar archivos de proyecto.

---
+++ Problema 3/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problema_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            float cp1, cp2, cp3, cef, ctf, nf;
+            Console.WriteLine("Ingrese su Primera Calificación Parcial: ");
+            cp1 = Convert.ToSingle(Console.ReadLine());
+            Console.WriteLine("Ingrese su Segunda Calificación Parcial: ");
+            cp2 = Convert.ToSingle(Console.ReadLine());
+            Console.WriteLine("Ingrese su Tercera Calificación Parcial: ");
+            cp3 = Convert.ToSingle(Console.ReadLine());
+            Console.WriteLine("Ingrese su Calificación del examen final: ");
+            cef = Convert.ToSingle(Console.ReadLine());
+            Console.WriteLine("Ingrese su Calificación del trabajo final: ");
+            ctf = Convert.ToSingle(Console.ReadLine());
+            nf = 0.55f * (cp1 + cp2 + cp3) / 3 + 0.30f * cef + 0.15f * ctf;
+            Console.WriteLine("El promedio final es: " + nf.ToString("0.00"));
+            Console.ReadKey();
+
+        }
+    }
+}
